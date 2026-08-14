eba4250dd9303fdc27d49d745b483c2d0350bd6d
Your Name <vaibhavkrgupta369@gmail.com>
2026-08-14 13:45:11 +0530

    feat(storage): configure supabase storage backend

--- backend/vault/storage.py
+++ backend/vault/storage.py
@@ -1,4 +1,12 @@
-from django.core.files.storage import default_storage
+import os
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+from storages.backends.s3 import S3Storage
 
-class ResourceStorage(default_storage.__class__):
-    pass
+class ResourceStorage(S3Storage if getattr(settings, 'USE_S3', False) else FileSystemStorage):
+    def __init__(self, *args, **kwargs):
+        if getattr(settings, 'USE_S3', False):
+            kwargs['location'] = 'resources'
+        else:
+            kwargs['location'] = os.path.join(settings.MEDIA_ROOT, 'resources')
+        super().__init__(*args, **kwargs)
